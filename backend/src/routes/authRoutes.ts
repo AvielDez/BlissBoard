7be237d4080ac4a0471713097b6fedc33a990f46
avieldez <avielhernandez.01@gmail.com>
2024-07-20 23:41:00 -0500

    added token authentication to user requests

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { login, register, updateUsername } from "../controllers/authController";
+import { authenticateToken } from "../middleware/authMiddleware";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.put("/updateUsername/:userId", updateUsername);
+router.put("/updateUsername/:userId", authenticateToken, updateUsername);
 
 export default router;
